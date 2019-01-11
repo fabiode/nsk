eb93556e7d24d53d504846e17d2054540ad12a91
Fabio Daguer <fabiode@icloud.com>
2019-01-11 11:44:57 -0200

    Add staging verification for coupon limit in db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,9 @@ if Coupon.any?
   puts "DB Seed deactivated: Check if are any coupons on database."
 else
   require Rails.root.join('lib/coupon_generator.rb')
-  CouponGenerator.generate
+  if Rails.env.staging?
+    CouponGenerator.generate({ 0 => (0..100), 1 => (0..100)  })
+  else
+    CouponGenerator.generate
+  end
 end
