5fe165e9cd4fb52b05977970edf6405b92f7e5f4
fabiode <eu@fabiode.com.br>
2019-01-23 10:58:48 -0200

    add klasme + niina secrets as sender name

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   add_template_helper CouponsHelper
-  default from: 'nsk@klasme.com'
+  default from: 'Klasme + Niina Secrets <nsk@klasme.com>'
   layout 'mailer'
 end
