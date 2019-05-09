7ce108bb4430a0c32f3bab3b56527fcba3fdd3d7
Fabio Daguer <fabiode@icloud.com>
2019-05-09 19:27:17 -0300

    Add winner page, root is now winner page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :user, controllers: { registrations: 'users/registrations', sessions: 'users/sessions', passwords: 'users/passwords' }
-  root to: 'coupons#index'
 
   get 'p/:page', to: 'pages#show', as: :page
+  root to: 'pages#show', page: 'ganhador'
   resources :coupons, only: [:index] do
     post 'sync', on: :collection
   end
